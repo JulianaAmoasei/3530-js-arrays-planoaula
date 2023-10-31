35ec55b794a20bf0d55961ba3111f735e1b95dc4
JulianaAmoasei <late.parrot@gmail.com>
2023-10-31 14:47:01 -0300

    video: desestruturando lista

--- desestruturando-lista.js
+++ desestruturando-lista.js
@@ -5,22 +5,8 @@ const listaDeAlunosEMedias = [alunos, medias];
 
 function exibeNomeENota(aluno) {
   if (listaDeAlunosEMedias[0].includes(aluno)) {
-    const indice = listaDeAlunosEMedias[0].indexOf(aluno);
-
-    const mediaDoAluno = listaDeAlunosEMedias[1][indice];
-
-    console.log(`${aluno} tem a média ${mediaDoAluno}.`);
-  } else {
-    console.log("Aluno não encontrado!");
-  }
-}
-
-function exibeNomeENota(aluno) {
-  if (listaDeAlunosEMedias[0].includes(aluno)) {
-    const indice = listaDeAlunosEMedias[0].indexOf(aluno);
     const [alunos, medias] = listaDeAlunosEMedias;
 
-    const mediaDoAluno = listaDeAlunosEMedias[1][indice];
     const indice = alunos.indexOf(aluno);
 
     const mediaDoAluno = medias[indice];
