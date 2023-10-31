9e7ca95922747b3bf1981793f0290a30ef2977c0
JulianaAmoasei <late.parrot@gmail.com>
2023-10-31 14:56:41 -0300

    video: percorrendo com for

---
+++ percorrendo-for.js
@@ -0,0 +1,9 @@
+const notas = [10, 8.5, 5, 6.5, 8, 7.5];
+
+// Primeira expressão: é executada apenas uma única vez
+// Segunda expressão: condição de execução
+// Terceira expressão: é executada sempre ao final do bloco
+
+for (let indice = 0; indice < notas.length; indice++) {
+  console.log(indice, notas[indice]);
+};
