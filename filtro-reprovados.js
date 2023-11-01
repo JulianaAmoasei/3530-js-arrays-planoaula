5a9c37e0062a7dedf1b9e50777110d77bc4f90b4
JulianaAmoasei <late.parrot@gmail.com>
2023-11-01 12:20:47 -0300

    video: filtrando elementos

---
+++ filtro-reprovados.js
@@ -0,0 +1,12 @@
+const alunos = ["Ana", "Marcos", "Maria", "Mauro"];
+const medias = [7, 4.5, 8, 7.5];
+
+// const tamanhoNome = alunos.filter((aluno) => {
+//   return aluno.length < 4;
+// });
+
+const reprovados = alunos.filter((_, indice) => {
+  return medias[indice] < 7;
+});
+
+console.log(reprovados);
